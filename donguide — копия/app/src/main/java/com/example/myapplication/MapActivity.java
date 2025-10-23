1280ef24d3c645f9c2d53b18ff972b1abb4a95e5
iliasolop <isolop1446@gmail.com>
2025-10-23 14:47:51 +0300

    DOBAVILI PEREHODDDDDDDDD!!!!!!!!

---
+++ donguide — копия/app/src/main/java/com/example/myapplication/MapActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MapActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = new TextView(this);
+        textView.setText("Hello World!");
+
+        findViewById(R.id.button_map).setOnClickListener(this::onClickMapActivity);
+    }
+
+    private void onClickMapActivity(View view){
+        var intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+}
